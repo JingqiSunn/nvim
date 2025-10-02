f1622c1b1716605425baca9bda32c99a6cb59556
Jingqi Sunn <Jingqi.sun@outlook.com>
2025-10-02 17:52:42 +0800

    update

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -55,7 +55,10 @@ return require('packer').startup(function(use)
   use "windwp/nvim-autopairs" -- 自动补全括号
 
   use "akinsho/bufferline.nvim" -- buffer分割线
-  use "github/copilot.vim"
+  use {
+    "github/copilot.vim",
+    commit = "cd7f01009fb7b30e22840cadc4faad88b05c6eef"
+}
   use "lewis6991/gitsigns.nvim" -- 左则git提示
   use {"akinsho/toggleterm.nvim", tag = "*"}
   use {
